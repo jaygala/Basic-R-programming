5e83bfafc86466c2188285f2c54a397e013017e2
2017A7PS0396G <f20170396@goa.bits-pilani.ac.in>
2018-09-15 23:02:46 +0530

    twitter and word cloud

---
+++ word_cloud.R
@@ -0,0 +1,13 @@
+# USE THIS -- www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
+
+# Install
+install.packages('tm')
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
